7c0b4ce1cef7282c6508df74a54c6efdca02bc31
Kyrylo Zakharov <kirillzakharov13@gmail.com>
2026-09-16 15:15:56 +0300

    Change in birth certificate for Excel

--- src/validators/birth-certificate.ts
+++ src/validators/birth-certificate.ts
@@ -2,7 +2,7 @@ const validateBirthCertificate = (value: string | undefined): boolean => {
   if (value === undefined) throw new Error('Номер свідоцтва про народження не зазначений')
   const checkValue = String(value).trim().replace(/[^А-ЯІЄЇҐ0-9I-]/g, '')
 
-  if (/^I-[А-ЯІЄЇҐ]{2}\s{0,1}\d{6}$/g.test(checkValue)) return true
+  if (/^[IV]{1,3}-[А-ЯІЄЇҐ]{2}\s{0,1}\d{6}$/g.test(checkValue)) return true
 
   throw Error('Це невірний номер свідоцтва про народження')
 }
